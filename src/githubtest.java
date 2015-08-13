0b071a0a8420bcf42e6a74b298abaef0630e893e
sallypbn <345311487@qq,com>
2015-08-13 13:24:46 +0800

    two

--- src/githubtest.java
+++ src/githubtest.java
@@ -7,6 +7,7 @@ public class githubtest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("githubtest1");
+		System.out.println("githubtest2");
 	}
 
 }
